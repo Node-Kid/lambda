1facc55a7f379ed841859447a0bf4de71196a752
will weiszhaar <weiszhaarwill@gmail.com>
2023-06-20 23:11:39 -0400

    cleanup workspace

--- src/main/kotlin/com/lambda/client/module/modules/player/NoGhostBlocks.kt
+++ src/main/kotlin/com/lambda/client/module/modules/player/NoGhostBlocks.kt
@@ -1,6 +1,5 @@
 package com.lambda.client.module.modules.player
 
-import com.lambda.client.LambdaMod
 import com.lambda.client.event.events.BlockPlaceEvent
 import com.lambda.client.event.events.PacketEvent
 import com.lambda.client.event.events.RenderWorldEvent
